8c34d880afe9b2f7c1b9b356058e1a0fda6fb652
Benjamin-Coleman <github email address>
2017-08-28 11:34:50 -0400

    Done.

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,1 +1,58 @@
-// Code Product Component Here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Product extends React.Component {
+	render() {
+		return (<div>Product</div>)
+	}
+}
+
+Product.defaultProps = {
+	hasWatermark: false
+}
+
+// function weightRange(props, propName, componentName) {
+// 	if (props[propName]) {
+// 		let value = props[propName];
+// 		if (value < 80){
+// 			return new Error ('too light')
+// 		}
+// 		else if (value > 300) {
+// 			return new Error ('too heavy')
+// 		}
+// 		else 
+// 		{
+// 			return null
+// 		}
+// 	}
+// }
+
+Product.propTypes = {
+	name: PropTypes.string.isRequired,
+	producer: PropTypes.string,
+	hasWatermark: PropTypes.bool,
+	color: PropTypes.oneOf(['white', 'eggshell-white', 'salmon']).isRequired,
+	weight: function (props, propName, componentName) {
+		if (props[propName] === undefined){
+			return new Error('must be definied')
+		}
+		if (props[propName]) {
+			let value = props[propName];
+			if (isNaN(value)){
+				return new Error('must be a number')
+			}
+			else if (value < 80){
+				return new Error('too light')
+			}
+			else if (value > 300) {
+				return new Error('too heavy')
+			}
+			else 
+			{
+				return null
+			}
+		}
+	}
+}
+
+export default Product;
